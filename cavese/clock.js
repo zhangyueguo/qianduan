a54c385b3c03fca7757b989e9793a73e93e2acc1
yueguo <1156391296@qq.com>
2016-10-09 20:31:14 +0800

    tianjia shuzi

--- cavese/clock.js
+++ cavese/clock.js
@@ -15,6 +15,32 @@ function drawBrackground()
     ctx.lineWidth = 10;
     ctx.arc(0,0,r-5,0,2*Math.PI,false);
     ctx.stroke();
+
+    var hourNumbers = [3,4,5,6,7,8,9,10,11,12,1,2];
+    ctx.font = '18px Arial';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    hourNumbers.forEach(function(number,i){
+       var rad = 2*Math.PI / 12*i;
+        var x = Math.cos(rad) * (r - 30);
+        var y = Math.sin(rad) * (r - 30);
+        ctx.fillText(number,x,y);
+    });
+
+    for(var i=0;i<60;i++){
+        var rad = 2*Math.PI / 60*i;
+        var x = Math.cos(rad) * (r - 18);
+        var y =  Math.sin(rad) * (r - 18);
+        ctx.beginPath();
+        if(i%5 === 0){
+            ctx.fillStyle = '#000';
+            ctx.arc(x,y,2,0,2*Math.PI,false);
+        }else {
+            ctx.fillStyle = '#ccc';
+            ctx.arc(x,y,2,0,2*Math.PI,false);
+        }
+        ctx.fill();
+    }
 }
 
 drawBrackground();
